f1a38823994e16d0cb52cac4310d4e0fd1e6baac
Ales Komarek <ales-komarek@seznam.cz>
2024-12-02 18:53:25 +0100

    patch: capscolor

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "ales";
+static const char *group = "ales";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INPUT] =  "#009900",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
-	[CAPS] = "red",         /* CapsLock on */
+	[CAPS] = "#EE0000",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
